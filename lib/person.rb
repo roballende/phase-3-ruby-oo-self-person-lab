8dd82063550d3ddb71724c28c00ef205f502523c
Robert Allende <allende.rob@gmail.com>
2023-01-11 10:57:45 -0600

    commit tree b3d8164a461bc250c719b4dcd92b948899aef05f

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,40 @@
-# your code goes here
+require "pry"
+class Person
+    attr_accessor :bank_account, :hygiene
+    attr_reader :name, :happiness
+
+    
+
+    def initialize(name, happiness = 8)
+        @name = name
+        @bank_account = 25
+        @happiness = happiness
+        @hygiene = 8
+
+    end
+
+    def happiness=(value)
+        if value >= 10
+            @happiness = 10
+        elsif value <= 0
+            @happiness = 0
+        else 
+            @happiness = value
+        end
+    end
+
+    # def happiness=(happiness_level)
+    #     if happiness_level <= 10 && happiness_level >= 0
+    #         @happiness = happiness_level
+    #     end
+    # end
+
+
+
+    
+
+   
+end
+
+J = Person.new("John", 7)
+binding.pry
